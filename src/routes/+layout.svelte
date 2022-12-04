0f44684edd755709d441fc37fbfd33eab962848f
York <111544765+helloyork@users.noreply.github.com>
2022-12-03 22:21:51 -0800

    magicConch109

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
-<dev style="text-align:center;"><ul id="header">
-	<li id="header-element"><a class="active" href="#home">Home</a></li>
-	<li id="header-element"><a href="#news">news</a></li>
-	<li id="header-element"><a href="#contact">Contact Us</a></li>
-	<li id="header-element"><a href="#about">About Us</a></li>
-</ul></dev>
+<script>
+	import '../app.scss';
+</script>
+
+<dev style="text-align:center;"
+	><ul id="header">
+		<li id="header-element"><a class="active" href="#home">Home</a></li>
+		<li id="header-element"><a href="#news">news</a></li>
+		<li id="header-element"><a href="#contact">Contact Us</a></li>
+		<li id="header-element"><a href="#about">About Us</a></li>
+	</ul></dev
+>
 <slot />
 
 <style type="text/css">
@@ -13,28 +19,28 @@
 		padding: 0;
 		overflow: hidden;
 		background-color: rgba(255, 255, 255, 0);
-        width: 100vw;
-        float: center;
-        position: fixed;
+		width: 100vw;
+		float: center;
+		position: fixed;
 	}
 	#header-element {
-        display:inline-block;
+		display: inline-block;
 	}
 	#header-element a {
 		display: block;
 		color: white;
 		text-align: center;
-		padding: 14px 20px;
+		padding: 18px 25px;
 		text-decoration: none;
-        font-weight: 100;
+		font-weight: 100;
 	}
 	#header-element a:hover:not(.active) {
-		background-color: #1fc7a5;
-	}
-    #header-element .active:hover {
-		background-color: #1ca68b;
+		background-color: #ffffff0e;
 	}
 	.active {
-		background-color: #14ab8d;
+		background-color: rgba(0, 0, 0, 0.136);
+	}
+	.active:hover {
+		background-color: rgba(0, 0, 0, 0.053);
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,40 @@
+<script>
+</script>
+
 <title>Magic Conch!</title>
+
 <dev>
+	<!-- svelte-ignore a11y-missing-content -->
 	<h5 id="banner" />
 </dev>
+<div class="alert alert-warning" role="alert" style="width:98%;margin:auto;">
+	<strong>警告！</strong>这个网页正在施工，可能无法正常访问内容，请见谅
+</div>
+
+<div class="accordion" id="accordionExample" style="padding-top:10px;width:98%;margin:auto;">
+	<div class="accordion-item">
+		<h2 class="accordion-header" id="headingOne">
+			<button
+				class="accordion-button"
+				type="button"
+				data-bs-toggle="collapse"
+				data-bs-target="#collapseOne"
+				aria-expanded="true"
+				aria-controls="collapseOne"
+			>
+				我是标题
+			</button>
+		</h2>
+		<div
+			id="collapseOne"
+			class="accordion-collapse collapse show"
+			aria-labelledby="headingOne"
+			data-bs-parent="#accordionExample"
+		>
+			<div class="accordion-body">我是内容</div>
+		</div>
+	</div>
+</div>
 
 <style type="text/css">
 	#banner {
