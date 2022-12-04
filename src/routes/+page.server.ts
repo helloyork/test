d30633cb7b54d13fb9ddabe21c724900eb1a0726
York <111544765+helloyork@users.noreply.github.com>
2022-12-03 16:57:52 -0800

    magicConch104

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,10 @@
-import sqlite3 from 'sqlite3'
-import { open } from 'sqlite'
-
-open({
-	filename: '/tmp/database.db',
-	driver: sqlite3.Database
-}).then((db) => {
-	// do your thing
-})
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import type { PageServerLoad } from './$types';
+export const db=async()=>await open({
+	filename: '../../../test.db',
+	driver: sqlite3
+});
+export const load: PageServerLoad = async()=>{
+	(await db()).run
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,10 @@
 <title>awa</title>
 
 <script type="ts">
-	import { spring } from 'svelte/motion';
-	import type { PageData } from './$types';
-	export let data: PageData;
-    
-	let pos = spring(
-		{ x: 50, y: 50 },
-		{
-			stiffness: 0.1,
-			damping: 0.25
-		}
-	);
 
-	let size = spring(10);
 </script>
 
-<svg
-	on:mousemove={(e) => pos.set({ x: e.clientX, y: e.clientY })}
-	on:mousedown={() => size.set(30)}
-	on:mouseup={() => size.set(10)}
-	style="height: 100vh;width:100vw"
->
-	<circle fill="red" cx={$pos.x} cy={$pos.y} r={$size} />
-</svg>
+
 
 <h1>没错你正在访问的网站由神奇海螺运行</h1>
-
+<h6>Hello World!<h6/>
