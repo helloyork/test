49e27bb55bb6c3148843561f5753a10de1214f75
York <111544765+helloyork@users.noreply.github.com>
2022-12-03 20:44:39 -0800

    magicConch107

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,35 @@
+<dev style="text-align:center;"><ul id="header">
+	<li id="header-element"><a class="active" href="#home">Home</a></li>
+	<li id="header-element"><a href="#news">news</a></li>
+	<li id="header-element"><a href="#contact">Contact Us</a></li>
+	<li id="header-element"><a href="#about">About Us</a></li>
+</ul></dev>
+<slot />
 
-<slot/>
+<style>
+	#header {
+		list-style-type: none;
+		margin: 0;
+		padding: 0;
+		overflow: hidden;
+		background-color: #333;
+        width: 100vw;
+        float: center;
+	}
+	#header-element {
+        display:inline-block;
+	}
+	#header-element a {
+		display: block;
+		color: white;
+		text-align: center;
+		padding: 14px 20px;
+		text-decoration: none;
+	}
+	#header-element a:hover:not(.active) {
+		background-color: #111;
+	}
+	.active {
+		background-color: #4caf50;
+	}
+</style>
