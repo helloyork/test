15d0b873ba512fcd4f2da24fd2971b91671280bb
Alan Best Hacker <AlanBestHacker@outlook.com>
2022-12-05 11:46:15 +0800

    Co-authored-by: York <helloyork@users.noreply.github.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,50 +1,51 @@
 <script>
+	import '@mdi/font/css/materialdesignicons.min.css';
 	import 'fluent-svelte/theme.css';
+	import '../app.css';
+	const navItems = [
+		{ label: '关于我们', href: '/about' },
+		{ label: '联系我们', href: '/contact' },
+		{ label: 'Test', href: '/test' },
+		{ label: 'Test', href: '/test' }
+	];
+	let y = 0;
+	$: if (y >= 300) alert('hiya!');
 </script>
 
+<svelte:window bind:scrollY={y} />
+<div class="flex flex-col h-full">
+	<div
+		id="header-div"
+		class="h-12 bg-cyan-500 text-white flex justify-center flex-col space-x-4 px-5"
+		style="height: 60px;"
+	>
+		<div class="h-8 flex flex-row  items-center">
+			<span class="font-bold flex " style="margin-right: 20px;">
+				<a href="../" style="margin: 5px;padding:13px 25px;">Nomen 小队</a>
+			</span>
+			{#each navItems as item}
+				<a
+					href={item.href}
+					class="no-underline text-opacity-50 hover:text-opacity-80 transition p-2"
+					id="header-element"
+					style="margin: 5px;padding:13px 25px;">{item.label}</a
+				>
+			{/each}
+		</div>
+	</div>
+	<div class="flex flex-auto ">
+		<div class="container m-auto h-full">
+			<slot />
+		</div>
+	</div>
+</div>
 
-	<ul id="header" style="float: center;margin-top:0;">
-		<li id="header-element"><a class="active" href="../">Home</a></li>
-		<li id="header-element"><a href='../test'>test</a></li>
-		<li id="header-element"><a href="../contact">Contact Us</a></li>
-		<li id="header-element"><a href="../about">About Us</a></li>
-		<li id="header-element" class="header-element-last"><a href="../about">About Us</a></li>
-	</ul>
-
-<slot />
-
-<style type="text/css">
-	#header {
-		list-style-type: none;
-		padding: 0;
-		overflow: hidden;
-		background-color: rgba(255, 255, 255, 0);
-		width: 100vw;
-		float: center;
-		position: fixed;
+<style>
+	#header-element:hover {
+		color: rgb(180, 255, 241);
+		background-color: rgb(33, 222, 191);
 	}
-	#header-element {
-		display: inline-block;
-	}
-	#header-element a {
-		display: block;
-		color: white;
-		text-align: center;
-		padding: 18px 25px;
-		text-decoration: none;
-		font-weight: 100;
-	}
-	#header-element a:hover:not(.active) {
-		background-color: #ffffff0e;
-	}
-	.active {
-		background-color: rgba(0, 0, 0, 0.136);
-	}
-	.active:hover {
-		background-color: rgba(0, 0, 0, 0.053);
-	}
-	.header-element-last{
-		float: right;
-		background-color: #ffffff45;
+	#header-div {
+		background-color: rgba(34, 238, 204);
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,4 @@
-<script>
-	import 'fluent-svelte/theme.css';
-	import '@mdi/font/css/materialdesignicons.min.css'
+<script lang="ts">
 </script>
 
-<dev>
-	<!-- svelte-ignore a11y-missing-content -->
-	<h5 id="banner" />
-</dev>
-
-<style type="text/css">
-	#banner {
-		margin-top: 0%;
-		padding: 300px;
-		text-align: center;
-		background: #1abc9c;
-		font-size: 30px;
-	}
-</style>
+<!-- <div style="margin-bottom:2400px;"><h6>hiya!</h6></div> -->
