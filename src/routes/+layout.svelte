1f1170141a035a00c046ac6e8d6fca240318396f
York <111544765+helloyork@users.noreply.github.com>
2022-12-05 20:50:25 -0800

    i105恢复

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,38 +1,8 @@
 <script lang="ts">
-	//你有没有注意到一个神奇的语法错误？?
-	import { INavItem } from '$lib/Nav/type';
 	import '@mdi/font/css/materialdesignicons.min.css';
 	import 'fluent-svelte/theme.css';
 	import '../app.css';
-	import Nav from '../lib/Nav/Nav.svelte';
-	const navItems: INavItem = [
-		{ label: '关于我们', href: '/about' },
-		{ label: '联系我们', href: '/contact' },
-		{ label: 'Test', href: '/test' },
-		{
-			label: '资源',
-			href: '/storage',
-			children: [
-				{ label: '下北泽刊阅', href: 'https://1919810.com' },
-				{ label: 'te', href: '' },
-				{ label: 'te', href: '' },
-				{ label: 'te', href: '' },
-				{ label: 'te', href: '' },
-				{ label: 'te', href: '' },
-				{ label: 'te', href: '' },
-				{ label: 'te', href: '' }
-			]
-		},
-		{ label: 'Test', href: '/test' }
-	];
 </script>
 
-<div class="flex flex-col h-full">
-	<Nav {navItems} />
-	<div class="flex flex-auto ">
-		<div class="container m-auto h-full">
-			<slot />
-		</div>
-	</div>
-</div>
+
 
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,3 @@
 <script lang="ts">
 </script>
 
-<!-- <div style="margin-bottom:2400px;"><h6>hiya!</h6></div> -->
