4f255373325df266ee4c8c888c0d12b6412e4c0a
York <111544765+helloyork@users.noreply.github.com>
2022-12-06 20:34:50 -0800

    index-i106

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,10 @@
 	import '@mdi/font/css/materialdesignicons.min.css';
 	import 'fluent-svelte/theme.css';
 	import '../app.css';
+	import { onMount } from 'svelte';
+	onMount(()=>{
+
+	})
 </script>
 
 <nav class="bg-gray-800">
@@ -199,29 +203,71 @@
 	</div>
 </nav>
 
-
-<button id="dropdownInformationButton" data-dropdown-toggle="dropdownInformation" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown header <svg class="ml-2 w-4 h-4" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
+<button
+	id="dropdownInformationButton"
+	data-dropdown-toggle="dropdownInformation"
+	class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+	type="button"
+	>Dropdown header <svg
+		class="ml-2 w-4 h-4"
+		aria-hidden="true"
+		fill="none"
+		stroke="currentColor"
+		viewBox="0 0 24 24"
+		xmlns="http://www.w3.org/2000/svg"
+		><path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			stroke-width="2"
+			d="M19 9l-7 7-7-7"
+		/></svg
+	></button
+>
 
 <!-- Dropdown menu -->
-<div id="dropdownInformation" class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600">
-    <div class="py-3 px-4 text-sm text-gray-900 dark:text-white">
-      <div>Bonnie Green</div>
-      <div class="font-medium truncate">name@flowbite.com</div>
-    </div>
-    <ul class="py-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownInformationButton">
-      <li>
-        <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
-      </li>
-      <li>
-        <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
-      </li>
-      <li>
-        <a href="#" class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
-      </li>
-    </ul>
-    <div class="py-1">
-      <a href="#" class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign out</a>
-    </div>
+<div
+	id="dropdownInformation"
+	class="hidden z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600"
+>
+	<div class="py-3 px-4 text-sm text-gray-900 dark:text-white">
+		<div>Bonnie Green</div>
+		<div class="font-medium truncate">name@flowbite.com</div>
+	</div>
+	<ul
+		class="py-1 text-sm text-gray-700 dark:text-gray-200"
+		aria-labelledby="dropdownInformationButton"
+	>
+		<li>
+			<a
+				href="#"
+				class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+				>Dashboard</a
+			>
+		</li>
+		<li>
+			<a
+				href="#"
+				class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+				>Settings</a
+			>
+		</li>
+		<li>
+			<a
+				href="#"
+				class="block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+				>Earnings</a
+			>
+		</li>
+	</ul>
+	<div class="py-1">
+		<a
+			href="#"
+			class="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+			>Sign out</a
+		>
+	</div>
 </div>
 
-  <slot />
+
+
+<slot />
