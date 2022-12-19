6279f66393a1a677b435e20ebb22033d812d1de9
York <111544765+helloyork@users.noreply.github.com>
2022-12-18 21:42:10 -0800

    York-i100-1

--- src/lib/server/database/db.js
+++ src/lib/server/database/db.js
@@ -1,18 +1,17 @@
 
-// import sqlite3 from 'sqlite3';
-// let db = new sqlite3.Database('./user.db');
+import sqlite3 from 'sqlite3';
+let db = new sqlite3.Database('./user.db');
 export async function dbtest() {
-    let result;
-    let sql = `SELECT * FROM test`;
-    db.all(sql, [], (err, rows) => {
-        if (err) {
-            throw err;
-        }
-        rows.forEach((row) => {
-            result.push(row);
-        });
-    });
+    let result = await gDbtest();
+    console.log(result);
     return result;
-    // return 'hi!';
 }
-
+function gDbtest() {
+    return new Promise((resolve, reject) => {
+        let sql = `SELECT * FROM test2`;
+        db.all(sql, [], (err, row) => {
+            if (err) throw new Error(err);
+            resolve(row);
+        });
+    })
+}
--- src/routes/api/user/login/+server.ts
+++ src/routes/api/user/login/+server.ts
@@ -1,7 +1,8 @@
-import { json } from '@sveltejs/kit'
-
+import { dbtest } from '../../../../lib/server/database/db';
 /** @type {import('./$types').RequestHandler} */
 export async function POST({request}){
-    let message = await request.json();
+    // let message = await request.json();
+    let message = JSON.stringify(await dbtest());
+    // console.log(message);
     return new Response(JSON.stringify(message), { status: 200 })
 }
