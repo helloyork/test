ec616b15a3295387714317fc71da8f3887a58c9f
York <111544765+helloyork@users.noreply.github.com>
2022-12-24 20:16:07 -0800

    York-i103-1

--- src/lib/server/database/db.js
+++ src/lib/server/database/db.js
@@ -20,12 +20,11 @@ export async function login(name) {
 //CREATE TABLE user (userkey TEXT PRIMARY Key,username TEXT NOT NULL, password TEXT NOT NULL, nickname TEXT,value TEXT);
 export async function register({username,password,nickname,value={}}) {
     if ((await sql`SELECT * FROM user WHERE username = ${username}`).length>0) {
-        console.log('wth?')
         return { ok: false, message: '用户名已存在', code: 1 }
     }
     let r=await sql`INSERT INTO user VALUES(${md5('['+username+']user'+((await sql`SELECT * FROM user`).length))},${username},${md5(password)},${nickname},${JSON.stringify(value)})`;
     if(r.ok)return { ok: true, message: '成功', code: 0 };
-    throw new Error(r.result)
+    return { ok: false, message: '成功', code: 0 };
 }
 
 async function sql(cmd, ...params) {
