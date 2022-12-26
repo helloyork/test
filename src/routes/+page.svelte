37751f64abcec8209641fb5a7c95b7722ffba9b5
York <111544765+helloyork@users.noreply.github.com>
2022-12-25 20:01:01 -0800

    main

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,54 @@
 <script lang="ts">
 </script>
 
+<div class="isolate bg-white">
+	<main>
+		<div class="relative px-6 lg:px-8">
+			<div class="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+				<div>
+					<div class="hidden sm:mb-8 sm:flex sm:justify-center">
+						<!-- <div
+							class="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+						>
+							<span class="text-gray-600">
+								Announcing our next round of funding. <a
+									href="#"
+									class="font-semibold text-indigo-600"
+									><span class="absolute inset-0" aria-hidden="true" />Read more
+									<span aria-hidden="true">&rarr;</span></a
+								>
+							</span>
+						</div>  -->
+					</div>
+					<div>
+						<h1 class="text-4xl font-bold tracking-tight sm:text-center sm:text-5xl">
+							少年仍步青春时，引航明灯领前程
+						</h1>
+						<p class="mt-2 text-xl leading-9 text-gray-600 sm:text-center">
+							一个富有潜力的队伍
+						</p>
+						<div class="mt-20 flex gap-x-4 sm:justify-center">
+							<a
+								href="/about"
+								class="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-700 hover:bg-indigo-700 hover:ring-indigo-700"
+							>
+								加入我们
+								<span class="text-indigo-200" aria-hidden="true">&rarr;</span>
+							</a>
+							<a
+								href="/about"
+								class="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20"
+							>
+								联系我们
+							</a>
+						</div>
+					</div>
+					<div
+						class="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
+					>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</div>
