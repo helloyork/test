96ec0b34068da583256993602fcbacf46e21e053
York <111544765+helloyork@users.noreply.github.com>
2022-12-26 17:29:48 -0800

    York

--- src/lib/underline.svelte
+++ src/lib/underline.svelte
@@ -1,3 +1,6 @@
-<h1 class="inline-block underline decoration-indigo-500/50 decoration-8" href="#">
+<script>
+    export const href = '';
+</script>
+<a class="inline-block underline decoration-indigo-500/50 decoration-8 hover:text-black" {href}>
     <slot></slot>
-</h1>
+</a>
