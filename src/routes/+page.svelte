239762be5ed6a51bf569fbcf2d87cfe74e03a7a2
York <111544765+helloyork@users.noreply.github.com>
2022-12-27 16:52:14 -0800

    R

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -47,9 +47,9 @@
 						></svg
 					>
 				</span>
-				<p class="ml-3 truncate font-medium text-white align-middle">
+				<p class="ml-3 truncate font-medium text-white  items-center">
 					<span
-						class="md:hidden text-ellipsis flex"
+						class="md:hidden  items-center"
 						style="line-height: 50%; padding-top:50%; white-space: nowrap;"
 						>与我们加入网站的搭建！<a
 							class="text-white text-ellipsis inline-block underline decoration-indigo-500/70 decoration-8 pl-2 hover:text-gray-300"
@@ -57,8 +57,7 @@
 						></span
 					>
 					<span
-						class="hidden md:inline"
-						style="line-height: 50%; padding-top:50%; white-space: nowrap;"
+						class="hidden md:inline  items-center"
 						>我们需要你！加入网站的搭建，成为第一个得知新消息的人。 <a
 							class="text-white inline-block underline decoration-indigo-500/70 decoration-8 pl-2 hover:text-gray-300"
 							href="/about">与我们联系</a
