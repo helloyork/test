4383c71a9b9c1d03ca23eadeb2f7502e15ffdaf7
York <111544765+helloyork@users.noreply.github.com>
2022-12-31 14:57:04 -0800

    T

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,25 +1,4 @@
-<div class="main">
-	<h6 class="main text-gray-400">
-		页面不见了
-		如果你认为这是一个错误，请联系网站所有者
-		QQ:2418207411
-	</h6>
-	<!-- <img src="/404.png" alt="404 not Found" style="height: 50%;" /> -->
-</div>
-
-<style>
-	.main {
-		text-align: center; /*让div内部文字居中*/
-		background-color: #fff;
-		border-radius: 20px;
-		width: 300px;
-		height: 350px;
-		margin: auto;
-		position: absolute;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		margin-bottom: 80%;
-	}
-</style>
+<h6 class="mt-5 text-center text-black" style="margin-bottom:50vh;">
+	<strong>页面不见了</strong>
+	如果你认为这是一个错误，请联系网站所有者 QQ:2418207411
+</h6>
